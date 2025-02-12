d12bf0b2ef39531ae77af98d0bad2f9f9e24a061
Hussien <endrishussien313@gmail.com>
2025-02-12 12:01:18 +0300

    Netflix-clone-2025-deploy-last

--- netflix-react/src/components/Header/Header.jsx
+++ netflix-react/src/components/Header/Header.jsx
@@ -1,32 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.css";
 import Netflixlogo from "../../assets/image/Netflix_Logo_PMS.png";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import MenuIcon from "@mui/icons-material/Menu"; // Import hamburger icon
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className="header-outer-container">
-      <div className="header-container">
+    <>
+      <div className={`header-container ${show ? "header_black" : ""}`}>
         <div className="header-left">
           <ul>
-            <div>
+            <li>
               <img
                 className="Netflixlogo"
                 src={Netflixlogo}
                 alt="netflix logo"
                 width={100}
               />
-            </div>
-
-            <li>Home</li>
-            <li>TVShows</li>
-            <li>Movies</li>
-            <li>Latests</li>
-            <li>MyLists</li>
-            <li>Browse by Languages</li>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">TVShows</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">Latests</a>
+            </li>
+            <li>
+              <a href="#">MyLists</a>
+            </li>
+            <li>
+              <a href="#">Browse by Languages</a>
+            </li>
           </ul>
         </div>
 
@@ -41,7 +70,7 @@ const Header = () => {
             <li>
               <AccountBoxIcon />
             </li>
-            <div>
+            <li>
               <img
                 className="avatar"
                 src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
@@ -49,14 +78,15 @@ const Header = () => {
                 width="30"
                 height="30"
               />
-            </div>
+            </li>
             <li>
               <ArrowDropDownIcon />
             </li>
           </ul>
+         
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
